9daf74898c78e7d7c61e57a968702f44299947a6
Steve Dormady <stevedorm2022@gmail.com>
2025-11-26 21:39:28 -0500

    Pushing a test that takes over 7000 seconds to finish with exact

---
+++ exact_solution/test_cases/make_slow.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import itertools
+
+# 13 cities: A .. M
+nodes = [chr(ord('A') + i) for i in range(13)]  # ['A', 'B', ..., 'M']
+
+def weight(i, j):
+    # Deterministic symmetric weights in [1, 10]
+    return float(((i + 1) * (j + 3)) % 10 + 1)
+
+filename = "tc13_super_slow_13nodes.in"
+
+with open(filename, "w") as f:
+    n = len(nodes)
+    m = n * (n - 1) // 2
+    f.write(f"{n} {m}\n")
+    for i, j in itertools.combinations(range(n), 2):
+        u, v = nodes[i], nodes[j]
+        w = weight(i, j)
+        f.write(f"{u} {v} {w:.1f}\n")
+
+print(f"Wrote {filename}")
